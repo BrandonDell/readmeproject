08149388dfcede54769b30384747069558e9dd11
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-04-23 19:28:48 -0400

    finished generateMarkdown functions with Tutor Chris help debugging

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,3 +10,43 @@ function renderLicenseLink(license) {
   return license ? `* [License](#license)` : ``;
 } 
 
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) { 
+    return `
+    ## License
+    This project is licensed with ${license}`
+    }
+    
+    // TODO: Create a function to generate markdown for README
+    function generateMarkdown(data) {
+      return `# ${data.title} ${renderLicenseBadge(data.license)}
+      ## Description
+      ${data.description}
+      * [Installation](#installation)
+      * [Usage](#usage)
+      ${renderLicenseLink(data.license)}
+      * [Contributing](#contributing)
+      * [Test](#test)
+      * [Questions](#questions)
+      
+    //   ## Installation
+    //   ${data.installation}
+    
+    //   ## Usage
+    //   ${data.usage}
+    
+    //   ${renderLicenseSection(data.license)}
+    
+    //   ## Contributing
+    //   ${data.contributing}
+    
+    //   ## Test
+    //   ${data.test}
+    
+    //   ## Questions
+    //   ${data.questions}
+    // `;
+    }
+    module.exports = generateMarkdown;
+    
